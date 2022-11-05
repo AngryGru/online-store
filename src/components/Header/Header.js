e5ec188856170664901856c170649d416c32690f
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-11-05 14:40:55 +0300

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+import Logo from "../Logo/Logo";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Logo width={"70px"} height={"70px"} />
+      <h1 className="header-title">Geek's Alley</h1>
+    </div>
+  );
+};
+
+export default Header;
