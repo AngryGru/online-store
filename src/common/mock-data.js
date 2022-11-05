d3e9a5bac5316d4c57b04ff09580aa99ddeb0b06
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-11-05 14:41:25 +0300

    add mock-data

---
+++ src/common/mock-data.js
@@ -0,0 +1,67 @@
+const MOCK_DATA = [
+  {
+    id: 1,
+    image: "../assets/hogwarts-legacy.png",
+    title: "Hogwarts Legacy",
+    description:
+      "Genres: Action, Adventure, Role-playing | Release date: 10.02.2023 | 12+",
+    cost: "92$",
+  },
+  {
+    id: 2,
+    image: "../assets/rdr2.png",
+    title: "Red Dead Redemption",
+    description: "Genres: Action, Open World | Release Date: 26.10.2018 | 18+",
+    cost: "38$",
+  },
+  {
+    id: 3,
+    image: "../assets/spider-man.png",
+    title: "Marvel's Spider-Man",
+    description: "Genres: Action | Release date: 28.08.2019 | 18+",
+    cost: "32$",
+  },
+  {
+    id: 4,
+    image: "../assets/last-of-us-2.png",
+    title: "Last of us 2 Special edition",
+    description: "Genres: Action | Release Date: 19.06.2020 | 18+",
+    cost: "58$",
+  },
+  {
+    id: 5,
+    image: "../assets/cp77.png",
+    title: "Cyberpunk 2077",
+    description:
+      "Genres: Action, Adventure, Open World | Release date 10.12.2020 | 18+",
+    cost: "64$",
+  },
+  {
+    id: 6,
+    image: "../assets/cb4.png",
+    title: "CRASH BANDICOOT 4: ЭТО ВОПРОС ВРЕМЕНИ",
+    description: "Genres: Adventure | Release Date: 02.10.20 | 6+",
+    cost: "60$",
+  },
+  {
+    id: 7,
+    image: "../assets/resident-evil-2.png",
+    title: "Resident Evil 2",
+    description: "Genres: Action, Thriller | Release Date: 25.01.2019 | 18+",
+    cost: "48$",
+  },
+  {
+    id: 8,
+    image: "../assets/darksouls.png",
+    title: "Dark Souls Remastered",
+    description: "Genres: Role-playing | Release Date: 25.05.2018 | 16+",
+    cost: "34$",
+  },
+  {
+    id: 9,
+    image: "../assets/ghost.png",
+    title: "The Ghost of Tsushima Director's version",
+    description: "Genres: Action, Adventure | Release Date: 20.08.2021 | 18+",
+    cost: "59$",
+  },
+];
