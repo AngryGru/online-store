9bced7b412d5e576abed917f7a684225a4fdbe4f
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-11-05 18:44:13 +0300

    fix mock-data images

--- src/common/mock-data.js
+++ src/common/mock-data.js
@@ -1,36 +1,37 @@
-const MOCK_DATA = [
+export const MOCK_DATA = [
   {
     id: 1,
-    image: "../assets/hogwarts-legacy.png",
+    image: "https://www.belconsole.by/pics/items/20220906120952380_ps4.jpg",
     title: "Hogwarts Legacy",
     description:
-      "Genres: Action, Adventure, Role-playing | Release date: 10.02.2023 | 12+",
+      "Genres: Action, Adventure, Role-playing | Release date: 10.02.2023 | 16+",
     cost: "92$",
   },
   {
     id: 2,
-    image: "../assets/rdr2.png",
+    image: "https://www.belconsole.by/pics/items/rdr2_fob_ps4_rus.jpg",
     title: "Red Dead Redemption",
     description: "Genres: Action, Open World | Release Date: 26.10.2018 | 18+",
     cost: "38$",
   },
   {
     id: 3,
-    image: "../assets/spider-man.png",
+    image: "https://www.belconsole.by/pics/items/spider_man_ps4_cover.jpg",
     title: "Marvel's Spider-Man",
     description: "Genres: Action | Release date: 28.08.2019 | 18+",
     cost: "32$",
   },
   {
     id: 4,
-    image: "../assets/last-of-us-2.png",
+    image:
+      "https://www.belconsole.by/pics/items/last-of-us-2-special-edition.jpg",
     title: "Last of us 2 Special edition",
     description: "Genres: Action | Release Date: 19.06.2020 | 18+",
     cost: "58$",
   },
   {
     id: 5,
-    image: "../assets/cp77.png",
+    image: "https://www.belconsole.by/pics/items/cp77_ps4_2d_ru_a.jpg",
     title: "Cyberpunk 2077",
     description:
       "Genres: Action, Adventure, Open World | Release date 10.12.2020 | 18+",
@@ -38,28 +39,32 @@ const MOCK_DATA = [
   },
   {
     id: 6,
-    image: "../assets/cb4.png",
+    image:
+      "https://www.belconsole.by/pics/items/cb4_ps4_reveal_packshot_f_ru.jpg",
     title: "CRASH BANDICOOT 4: ЭТО ВОПРОС ВРЕМЕНИ",
     description: "Genres: Adventure | Release Date: 02.10.20 | 6+",
     cost: "60$",
   },
   {
     id: 7,
-    image: "../assets/resident-evil-2.png",
+    image:
+      "https://www.belconsole.by/pics/items/610e0352774d09f68400be22e03550f9_b.jpg",
     title: "Resident Evil 2",
     description: "Genres: Action, Thriller | Release Date: 25.01.2019 | 18+",
     cost: "48$",
   },
   {
     id: 8,
-    image: "../assets/darksouls.png",
+    image:
+      "https://www.belconsole.by/pics/items/darksouls_remastered_ps4_packshot_pegiprov_preview.jpg",
     title: "Dark Souls Remastered",
     description: "Genres: Role-playing | Release Date: 25.05.2018 | 16+",
     cost: "34$",
   },
   {
     id: 9,
-    image: "../assets/ghost.png",
+    image:
+      "https://www.belconsole.by/pics/items/ps4_gotdc_ste_pksht_ft_rgb_ru_210709.jpg",
     title: "The Ghost of Tsushima Director's version",
     description: "Genres: Action, Adventure | Release Date: 20.08.2021 | 18+",
     cost: "59$",
